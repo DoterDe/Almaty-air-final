3779e3afee98de7bd3e34a88a10704437a56275f
Doter <chil.aboka2008@gmail.com>
2025-11-26 03:29:58 +0500

    Обновил Vite билд и конфигурацию

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,6 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
+import react from '@vitejs/plugin-react'; // ← измените эту строку
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist',
-  },
-  base: '/' 
 });
